1737bd256ef58df4fad8fd2beabceab6b88065de
DimaAdmin <ryba-mech@yandex.ru>
2018-05-05 11:37:19 +0300

    04- complete & work app-demo-form-sku-with-builder

--- src/app/demo-form-sku-with-builder/demo-form-sku-with-builder.component.ts
+++ src/app/demo-form-sku-with-builder/demo-form-sku-with-builder.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-demo-form-sku-with-builder',
