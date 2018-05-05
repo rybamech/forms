399cdb727cb802b0c6bf35ee0e019372ad53c4b9
DimaAdmin <ryba-mech@yandex.ru>
2018-05-05 10:17:11 +0300

    Create new component demo-form-sku-with-builder

---
+++ src/app/demo-form-sku-with-builder/demo-form-sku-with-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-sku-with-builder',
+  templateUrl: './demo-form-sku-with-builder.component.html',
+  styleUrls: ['./demo-form-sku-with-builder.component.css']
+})
+export class DemoFormSkuWithBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
