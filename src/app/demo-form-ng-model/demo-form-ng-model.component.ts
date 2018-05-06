7232971ceb7cdab2991d5a9cdd2d8e44c8b419f4
DimaAdmin <ryba-mech@yandex.ru>
2018-05-06 12:19:24 +0300

    17- create demo-form-ng-model component

---
+++ src/app/demo-form-ng-model/demo-form-ng-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-ng-model',
+  templateUrl: './demo-form-ng-model.component.html',
+  styleUrls: ['./demo-form-ng-model.component.css']
+})
+export class DemoFormNgModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
